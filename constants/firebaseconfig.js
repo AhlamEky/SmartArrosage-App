afe355030c9078c04dc4114eccab10f6b4252270
DreamsEky <153957835+AhlamEky@users.noreply.github.com>
2025-11-13 17:51:09 +0100

    Update firebaseconfig.js

--- constants/firebaseconfig.js
+++ constants/firebaseconfig.js
@@ -32,18 +32,6 @@ const firebaseConfig = {
   appId: FIREBASE_APP_ID,
   measurementId: FIREBASE_MEASUREMENT_ID,
 };
-//✅ Ton vrai projet Firebase analytique :
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDkIeKM32iqK1IdIjb6WB3v1POPealWHNY",
-  authDomain: "arrosageintelligent.firebaseapp.com",
-  databaseURL: "https://arrosageintelligent-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "arrosageintelligent",
-  storageBucket: "arrosageintelligent.firebasestorage.app",
-  //storageBucket: "arrosageintelligent.appspot.com",
-  messagingSenderId: "964040889499",
-  appId: "1:964040889499:web:a4ac93f38d5797d7c8223e",
-  measurementId: "G-P3GWGNE99V"
-};*/
 
 // 🧠 Vérifie si Firebase est déjà initialisé (important pour Expo)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
