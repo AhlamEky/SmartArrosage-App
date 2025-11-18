465424cd9a5918051a93ea6732fd922a0600d978
AhlamEky <dreamseky@gmail.com>
2025-11-18 22:45:25 +0100

    modification in package

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,4 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
 
-app.listen(port, () => {
-  console.log(`Backend running on port ${port}`);
-});
+module.exports = app;
