db123ccbfa0bd79114ffc4e74777d8452e33493a
AhlamEky <dreamseky@gmail.com>
2025-11-19 15:24:33 +0100

    vercel

---
+++ backend/api/toggle-pump.js
@@ -0,0 +1,16 @@
+import { db } from "./_firebaseAdmin";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST")
+    return res.status(405).json({ error: "Méthode non autorisée" });
+
+  const { uid, status } = req.body;
+
+  if (!uid) return res.status(400).json({ error: "UID manquant" });
+
+  await db.ref(`users/${uid}/sensor`).update({
+    pumpStatus: status,
+  });
+
+  return res.status(200).json({ success: true });
+}
