87652595ad4f8961a164a407b13fc88557c500d2
AhlamEky <dreamseky@gmail.com>
2025-11-19 17:02:41 +0100

    vercel

--- backend/api/_firebaseAdmin.js
+++ backend/api/_firebaseAdmin.js
@@ -1,4 +1,4 @@
-import admin from "firebase-admin";
+/*import admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
@@ -8,6 +8,21 @@ if (!admin.apps.length) {
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+}
+
+export const db = admin.database();
+export const auth = admin.auth();
+
+*/
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL, // Assurez-vous que cette variable est définie dans Vercel aussi
   });
 }
 
